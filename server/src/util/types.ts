1ff3f2d20790df980ee60ec7f33b4196104ad4b8
Ron <ron.fried15@gmail.com>
2023-03-07 22:35:40 +0200

    fix conflicts

--- server/src/util/types.ts
+++ server/src/util/types.ts
@@ -23,10 +23,7 @@ export interface User {
   id: string;
   username: string;
   image: string;
-<<<<<<< HEAD
-=======
   email: string;
   emailVerified: boolean;
   name: string;
->>>>>>> b42569cf8653b1a675da9b7b8a9b7ef1d911de1c
 }
